1fb3f18ee5fc8961a3f82a04277fe2b09c33a087
Schwannden Kuo <schwannden@mobagel.com>
2022-11-25 18:09:03 +0800

    update greeting

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/schwannden/hello
 
 go 1.19
 
-require github.com/schwannden/greetings v0.0.0-20221125092553-0e9ef2019cf5
+require github.com/schwannden/greetings v0.0.4
--- hello.go
+++ hello.go
@@ -2,12 +2,30 @@ package main
 
 import (
     "fmt"
+    "log"
 
     "github.com/schwannden/greetings"
 )
 
 func main() {
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
     // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+    message, err := greetings.Hello("Schwannden")
+    // if error occur, print error message and exit
+    if err != nil {
+        log.Fatal(err)
+    }
     fmt.Println(message)
+
+    names := []string{"Gladys", "Samantha", "Darrin"}
+    messages, err := greetings.Hellos(names)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(messages)
 }
